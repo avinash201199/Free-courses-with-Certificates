55d581f22407af02a67d1e745f28d7a6ddfae92b
Avinash Singh <avinash201199@gmail.com>
2022-10-02 03:31:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ const course_details = {
                     "course_description": "Become an Android Developer from Scratch",
                     "link": "https://www.udemy.com/course/become-an-android-developer-from-scratch",
                     "tags": "android,scratch"
-                }
+                },
                 {
                     "website_name": "Udemy",
                     "course_description": "Essential Non Technical Skills of Effective Data Scientists",
                     "link": "https://www.udemy.com/share/104fI23@HXPESfaal6TkxBGwZVO4s8bsVUcn2pCEvhFwpop5i0kIQeGdP4BlxQn6kiJdDGm_6w==/",
                     "tags": "Data Scientists, Begineers"
-                }
+                },
 
             ]
         },
