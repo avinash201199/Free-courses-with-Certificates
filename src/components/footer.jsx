fe4b0a5ca5a6a0d1861e070937b13a17b4649e01
Avinash Singh <avinash201199@gmail.com>
2022-10-18 13:28:19 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footer'>
-      Made with <i class="fa fa-heart" style={{color: 'red'}}></i> <i class="fa fa-linkedin"></i><a target='_blank' href='https://www.linkedin.com/company/lets-code-forever/'>v4nshhh</a>
+      Made with <i class="fa fa-heart" style={{color: 'red'}}></i> <a target='_blank' href='https://www.linkedin.com/company/lets-code-forever/'><i class="fa fa-linkedin"></i></a>
     </div
   )
 }
