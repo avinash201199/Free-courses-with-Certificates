f99c4ddbecc588f4e544a1b85fede6425788f348
Avinash Singh <avinash201199@gmail.com>
2022-11-11 10:51:38 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footer'>
-      Made by Avinash Singh <a target='_blank' href='https://www.linkedin.com/company/lets-code-forever/'><i class="fa fa-linkedin-square"></i></a>
+      Made by Avinash & Vansh<a target='_blank' href='https://www.linkedin.com/company/lets-code-forever/'><i class="fa fa-linkedin-square"></i></a>
     </div>
   )
 }
