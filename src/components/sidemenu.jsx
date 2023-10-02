738d7cedac3ba27bd262396a969c27b0bdeafa2c
Dhruvn-patel <dhruvpateldhruvpatel01669@gmail.com>
2023-10-02 14:14:08 +0530

    Fix toogleMenu in Mobile Display

--- src/components/sidemenu.jsx
+++ src/components/sidemenu.jsx
@@ -7,10 +7,13 @@ import "../styles/sidemenu.css";
 export default function sidemenu(props) {
   const isActive = props.active;
 
+  const handleToogleSideMenu=()=>{
+    props.toggleSideMenu()
+  }
   return (
     <div className={isActive ? "active" : "inactive"}>
       <div className="sidemenu-bar">
-        <div className="sidemenu-close" onClick={props.toggleSideMenu}>
+        <div className="sidemenu-close" onClick={handleToogleSideMenu}>
           <X />
         </div>
         <div className="sidemenu-bar-title">
@@ -21,28 +24,28 @@ export default function sidemenu(props) {
           <span className="material-symbols-outlined">code_off</span>
         </div>
         <ul className="sidemenu-bar-list">
-          <Link to="/dashboard">
-            <li className="sidemenu-bar-items">
+          <Link to="/dashboard" >
+            <li className="sidemenu-bar-items" onClick={handleToogleSideMenu}>
               <span className="material-symbols-outlined">dashboard</span>
               Dashboard
             </li>
           </Link>
           <Link to="/">
-            <li className="sidemenu-bar-items">
+            <li className="sidemenu-bar-items" onClick={handleToogleSideMenu}>
               <span className="material-symbols-outlined">book</span>
               Courses
             </li>
           </Link>
-          <li className="sidemenu-bar-items">
+          <li className="sidemenu-bar-items" onClick={handleToogleSideMenu}>
             <span className="material-symbols-outlined">person</span>
             Students
           </li>
-          <li className="sidemenu-bar-items">
+          <li className="sidemenu-bar-items" onClick={handleToogleSideMenu}>
             <span className="material-symbols-outlined">library_books</span>
             Library
           </li>
           <Link to="/about">
-            <li className="sidemenu-bar-items">
+            <li className="sidemenu-bar-items" onClick={handleToogleSideMenu}>
               <span className="material-symbols-outlined">
                 sentiment_very_satisfied
               </span>
